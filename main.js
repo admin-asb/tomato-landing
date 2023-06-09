8c1b39b7676ce7e39acaf518bcb93d9432c93923
admin-asb <asb210297@gmail.com>
2023-06-09 14:51:31 +0600

    Added modal

--- main.js
+++ main.js
@@ -67,3 +67,29 @@ allPics.forEach(function (picture) {
   picturesObserver.observe(picture);
   picture.classList.add("picture--hidden");
 });
+
+/* Modal */
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnOpenModal = document.querySelector(".btn");
+const btnCloseModal = document.querySelector(".close-modal");
+
+const openModal = () => {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnOpenModal.addEventListener("click", openModal);
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
